22a5b48f963eac813ad67b8f2682b8174ff62652
Jonas Kalderstam <jonas@kalderstam.se>
2013-01-30 18:44:27 +0100

    dbhandler-blank
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- src/com/example/providerexample/database/DatabaseHandler.java
+++ src/com/example/providerexample/database/DatabaseHandler.java
@@ -2,12 +2,19 @@ package com.example.providerexample.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "providerExample";
+	
+	private final Context context;
 
+	public DatabaseHandler(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		this.context = context;
+	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
