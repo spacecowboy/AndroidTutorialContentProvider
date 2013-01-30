90e814529f7234e81e2fe87205cd4bcdb0ee6af7
Jonas Kalderstam <jonas@kalderstam.se>
2013-01-30 18:36:51 +0100

    dbhandler stub
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

---
+++ src/com/example/providerexample/database/DatabaseHandler.java
@@ -0,0 +1,24 @@
+package com.example.providerexample.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
